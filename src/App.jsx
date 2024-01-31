18a8ee8d3c9d3b92d49f893a38d37c77114a8da8
min-bok <leehy0782@gmail.com>
2024-01-31 18:27:09 +0900

    🎉 Init: 프로젝트 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BasicButton from "./components/Button/BasicButton";
+import GlobalStyle from "./style/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <BasicButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button/BasicButton.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BasicButton() {
+  return <div>BasicButton</div>;
+}
